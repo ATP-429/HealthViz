d1486a9f01644f3cc8c4e52a301a99873d582e2c
ATP-429 <atpgaming123@gmail.com>
2024-04-12 21:43:39 +0530

    Add stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import useScript from './hooks/useScript';
+import styles from './style.css';
 
 function App() {
+  useScript('https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="form-box">
+      <div class="form">
+                            <form class="login-form">
+                                <center><h1 class="main-heading">Login Form</h1></center>
+				                <input id="user" type="text"placeholder="user name"/>
+				                <input id="pass" type="password" placeholder="password"/>
+				                <button id = "login-button">LOGIN</button>
+				                <p class="message">Not Registered? <a href="#">Register</a></p>
+				            </form>
+                            <form class="register-form">
+                                <center><h1 class="main-heading">Register Form</h1></center>
+				                <input type="text" placeholder="user name"/>
+				                <input type="text" placeholder="email-id"/>
+				                <input type="password" placeholder="password"/>
+				                <button>REGISTER</button>
+				                <p class="message">Already Registered?<a href="#">Login</a>
+				                </p>
+				            </form>
+			             </div>
+                   </div>
+    <df-messenger
+      chat-title="HealthViz-1"
+      agent-id="b9f0a9b6-d000-41a8-aba7-ae8d78134912"
+      language-code="en"
+    ></df-messenger>
     </div>
   );
 }
